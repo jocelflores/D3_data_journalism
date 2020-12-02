293cba37ae22b8b5f17a015c733a6fcd4a29e312
jocelflores <jocelynflores@utexas.edu>
2020-12-01 23:23:19 -0600

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,10 +6,10 @@ var svgHeight = 660;
 
 // Define the chart's margins as an object
 var chartMargin = {
-  top: 30,
-  right: 30,
-  bottom: 30,
-  left: 30
+  top: 25,
+  right: 25,
+  bottom: 25,
+  left: 25
 };
 
 // Define dimensions of the chart area
@@ -43,9 +43,11 @@ d3.csv("data.csv").then(function (Data) {
         .domain([0, d3.max(Data, d => d.healthcare)])
         .range([chartHeight, 0]);
 
+    // Create chart axis
     var bottomAxis = d3.axisBottom(xScale);
     var leftAxis = d3.axisLeft(yScale);
 
+    // create bottom and left axis
     chartGroup.append("g")
         .call(leftAxis);
     
@@ -53,15 +55,68 @@ d3.csv("data.csv").then(function (Data) {
         .attr("transform", `translate(0, ${chartHeight})`)
         .call(bottomAxis);
     
-    chartGroup.selectAll("circle")
+    // create scatter circle plot for each point
+    var scatterPlot = chartGroup.selectAll("circle")
         .data(Data)
         .enter()
         .append("circle")
-        .attr("cx", d => xScale(d.poverty))
+        .attr("cx", (d, i) => xScale(d.poverty))
         .attr("cy", d => yScale(d.healthcare))
-        .attr("r", 5)
-        .attr("fill", "blue")
-        .attr("opacity", ".6");
+        .attr("r", 10)
+        .attr("fill", "red")
+        .attr("opacity", ".4")
+
+    // States
+    chartGroup.selectAll('text')
+        .data(Data)
+        .enter()
+        .append('text')
+        .attr('x', (d, i) => xScale(d.poverty))
+        .attr('y', d => (yScale(d.healthcare-0.2)))
+        .classed('label', true)
+        .text(d => d.abbr)
+        .on('mouseover', function(d) {
+            toolTip.show(d);
+        })
+        .on('mouseout', function(d) {
+            toolTip.hide(d, i);
+        });
+
+    // labels on axis
+    chartGroup.append('text')
+        .attr('transform', "rotate(90)")
+        .attr('y', 0 - chartMargin.left)
+        .attr('x', 0 - chartHeight)
+        .attr('dy', 'em')
+        .classed('Text1', true)
+        .attr('xAxisLabel', "Healthcare")
+        .text('Without Healthcare (%)');
+
+    chartGroup.append('text')
+        .attr('transform', "translate('+ chartWidth/2)+50'")
+        .classed('Text1', true)
+        .attr('xAxisLabel', "Healthcare")
+        .text('Poverty (%)');
+
+    // tooltip
+
+    var toolTip = d3.tip()
+        .attr('class', 'toolTip')
+        .offset([-10, 10])
+        .html(function(d) {
+            return(`${d.abbr}<br>Healthcare (%)<br>Poverty: ${d.poverty}`);
+            
+        });
+
+    chartGroup.call(toolTip);
+
+    scatterPlot.on('mouseover', function(d) {
+        toolTip.show(d);
+    })
+        .on('mouseout', function(d, i) {
+            toolTip.hide(d)
+        })
+   
 
 }).catch(function(error) {
     console.log(error);
